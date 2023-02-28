a3b4d0975194e2d60469961cd0715b78fef3c888
Ani0711 <106262026+Ani0711@users.noreply.github.com>
2023-02-28 09:34:27 +0530

    Update suminlist.dart

--- suminlist.dart
+++ suminlist.dart
@@ -4,3 +4,9 @@ void main(List<String> args) {
   List<int> list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   print(list.reduce((value, element) => value + element));
 }
+
+///for each is used for traversing and print
+///(reduce function)-----list.reduce((int accumulator ,int data) =>data +accumulator);
+///(=>) fat arrow means  the body of the function start but must be single line and it is also return the value in the statements called lambda function
+/// funciton in function are call back function
+
